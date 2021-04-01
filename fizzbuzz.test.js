9ef9ae6b3ad58eaf6f85ba9185a440b1a1dc8fcb
NulledCodeDev <oscarfdo2004@gmail.com>
2021-03-31 23:11:07 -0500

    Updated  fizzbuzz.test.js

--- fizzbuzz.test.js
+++ fizzbuzz.test.js
@@ -1,6 +1,7 @@
 const fizzbuzz = require('./fizzbuzz');
 
 describe("fizzbuzz", () => {
+    test("should print ")
     test("should print 1 if they receive 1", () => {
         const expected = 1;
         const result = fizzbuzz(1)
