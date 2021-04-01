bfaf5c7d3763d90af0dbd0dfb706937a6ddc02cd
NulledCodeDev <oscarfdo2004@gmail.com>
2021-03-31 22:56:42 -0500

    Updated  fizzbuzz.test.js

--- fizzbuzz.test.js
+++ fizzbuzz.test.js
@@ -32,6 +32,7 @@ describe("fizzbuzz", () => {
     });
 
     test("should print fizzbuzz if they receive a multiple of 3 and 5", () => {
-        
+        const expected = "fizzbuzz";
+        const result = fizzbuzz()
     })
 });
