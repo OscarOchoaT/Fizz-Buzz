eea7854ef9ec8e2341257727abecc9dd23f0da08
NulledCodeDev <oscarfdo2004@gmail.com>
2021-03-31 22:58:16 -0500

    Updated  fizzbuzz.js

--- fizzbuzz.js
+++ fizzbuzz.js
@@ -1,5 +1,7 @@
 function fizzbuzz(num) {
-    if(num % 3 == 0 && num % 5 == 0)
+    if(num % 3 == 0 && num % 5 == 0) {
+        return "fizzbuzz";
+    }
     if (num % 3 == 0) {
         return "fizz";
     }
