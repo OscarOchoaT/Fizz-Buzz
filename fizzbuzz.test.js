35ec08b803b24f0d5cb4d47bbfc9ed37e14a8a96
NulledCodeDev <oscarfdo2004@gmail.com>
2021-03-31 22:49:36 -0500

    Updated  fizzbuzz.test.js

--- fizzbuzz.test.js
+++ fizzbuzz.test.js
@@ -1,17 +1,17 @@
 const fizzbuzz = require('./fizzbuzz');
 
 describe("fizzbuzz", () => {
-    test('should print 1 if they recive 1', () => {
+    test("should print 1 if they recive 1", () => {
         const expected = 1;
         const result = fizzbuzz(1)
         expect(expected).toBe(result);
     });
 
-    test('should print fizz if they recive 3', () => {
+    test("should print fizz if they recive 3", () => {
         const expected = "fizz";
         const result = fizzbuzz(3);
         expect(expected).toBe(result);
     });
 
-    test('should print fizz if they receive a multiple of 3', () =>)
+    test("should print fizz if they receive a multiple of 3", () =>)
 });
