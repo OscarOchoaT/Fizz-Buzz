6c63e77b122ac035b08e8911b005a91feff92d28
NulledCodeDev <oscarfdo2004@gmail.com>
2021-03-31 22:39:21 -0500

    Updated  fizzbuzz.test.js

--- fizzbuzz.test.js
+++ fizzbuzz.test.js
@@ -3,5 +3,5 @@ import fizzbuzz from './fizzbuzz';
 describe("fizzbuzz", () => {
     test('should print 1 if they recive 1', () => {
         const expected = 1;
-        const result 
+        const result = fizzbuzz
     })
